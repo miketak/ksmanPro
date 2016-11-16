0e82d880d10230b066f4ffd18018d1bc76dd3bd9
MikeTak - Michael Takrama <mtakrama@yahoo.com>
2016-11-16 03:35:39 -0600

    Database Connection

---
+++ KSManDataAccess/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSManDataAccess
+{
+    class DBConnection
+    {
+        internal static SqlConnection GetConnection()
+        {
+            // Connection to central database
+            var connString = @"Data Source=DESKTOP-O5U6AVG\SQLEXPRESS;Initial Catalog=ksmanDB;Integrated Security=True";
+            var conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
