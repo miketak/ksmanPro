b18a28bc24a6f6f003afacfcea87d51546f0d5e0
MikeTak - Michael Takrama <mtakrama@yahoo.com>
2016-11-15 23:42:52 -0600

    work on User Class

--- KSManDataAccess/User.cs
+++ KSManDataAccess/User.cs
@@ -9,5 +9,9 @@ namespace KSManDataAccess
     class User
     {
         public string Username { get; set; }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
     }
 }
