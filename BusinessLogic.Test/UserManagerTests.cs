fb55fac1423692d83b0cbd5b7ecbfe11d64a4fa6
MikeTak - Michael Takrama <mtakrama@yahoo.com>
2016-11-19 11:59:01 -0600

    User ManagerUpdates

---
+++ BusinessLogic.Test/UserManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using DataObjects;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class UserManagerTests
+    {
+        //[TestMethod]
+        //[Ignore]
+        //public void TryHashSHA256()
+        //{
+        //    var umg = new UserManager();
+        //    string hashedString = umg.HashSHA256("word");
+        //    Assert.AreEqual("98c1eb4ee93476743763878fcb96a25fbc9a175074d64004779ecb5242f645e6", hashedString);
+
+        //}
+
+        [TestMethod]
+        public void TryAuthentication()
+        {
+            var umg = new UserManager();
+
+            var userfromDB = umg.authenticateUser("SCSYMCX", "hello");
+            // Assert.AreEqual(expectedResult, userfromDB);
+            Assert.AreEqual(userfromDB.FirstName, "Martin");
+            Assert.AreEqual(userfromDB.LastName, "Cox");
+
+        }
+    }
+}
---
+++ BusinessLogic/UserManager.cs
@@ -0,0 +1,83 @@
+﻿using DataAccessLayer;
+using DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class UserManager
+    {
+
+        internal string HashSHA256(string source)   //change back to internal
+        {
+            var result = "";
+
+            // this logic is always the same for our purposes
+            // create a byte array (8 bit unsigned int)
+            byte[] data;
+
+            // Hash providers are all created with factory methods.
+            using (SHA256 sha256hash = SHA256.Create())
+            {
+                // hash the input
+                data = sha256hash.ComputeHash(Encoding.UTF8.GetBytes(source));
+            }
+
+            // use a stringbuilder to conserve memory
+            var s = new StringBuilder();
+
+            // loop through the bytes creating characts
+            for (int i = 0; i < data.Length; i++)
+            {
+                s.Append(data[i].ToString("x2"));
+            }
+
+            result = s.ToString();
+            return result;
+        }
+        
+        
+        public User authenticateUser(string username, string password)
+        {
+            User user = null;
+            
+            //Username & Password pre-validation
+            if (username.Length < 7 || username.Length > 20)
+            {
+                throw new ApplicationException("Invalid Username");
+            }
+            if (password.Length < 3) // we really need a better method
+            {                        // possibly a regex for complexity
+                throw new ApplicationException("Invalid Password");
+            }
+
+            try
+            {
+                if ( UserAccessor.verifyUsernameAndPassword(username, password) )
+                {
+                    password = null;
+                    // need to create a user object to use as an access token
+
+                    // get a user object
+                    user = UserAccessor.retrieveUserByUsername(username);
+                }
+                else
+                {
+                    throw new ApplicationException("Authentication Failed!");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return user;
+        }
+
+        
+    }
+}
